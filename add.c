df0000e540fe5307dead7c4d6174708846284fc7
tanu0009 <118241362+tanu0009@users.noreply.github.com>
2023-09-10 23:35:35 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+int main()
+{
+    int a,b; 
+    printf("Enter number a");
+    scanf("%d", &a);
+
+    printf("Enter number b");
+    scanf("%d", &b);
+    
+    return 0;
+}
